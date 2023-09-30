c0a9ce44c1d8fe41a3d998e225b5e0f995e591aa
Festus1914 <oladiplentyfestus@gmail.com>
2023-09-30 13:47:57 +0000

    Initial commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,24 +3,19 @@ def pascal_triangle(n):
         return []
 
     triangle = []
-
     for i in range(n):
         row = [1]  # The first element in each row is always 1
 
-        if triangle:
-            # If there are previous rows, calculate the current row based on the previous row
-            prev_row = triangle[-1]
+        # Calculate the middle elements of the row
+        if i > 0:
+            prev_row = triangle[i - 1]
             for j in range(1, i):
                 row.append(prev_row[j - 1] + prev_row[j])
 
-            row.append(1)  # The last element in each row is always 1
+        # The last element in each row is always 1
+        if i > 0:
+            row.append(1)
 
         triangle.append(row)
 
     return triangle
-
-# Example usage:
-if __name__ == "__main__":
-    result = pascal_triangle(5)
-    for row in result:
-        print(row)
